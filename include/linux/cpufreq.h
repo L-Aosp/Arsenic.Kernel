fcfc8a3ccfe213237c3cb1cc24fd2bec693f2c9d
CheckYourScreen <fictitiousguy@yahoo.com>
2016-08-08 23:26:34 +0200

    Revert "Revert "Move get_cpu_idle_time() to cpufreq.c""
    This reverts commit 6ee8f60ebd3a5ab7eafc5b2a0e9557557a887a06.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -318,6 +318,7 @@ __ATTR(_name, 0644, show_##_name, store_##_name)
 /*********************************************************************
  *                        CPUFREQ 2.6. INTERFACE                     *
  *********************************************************************/
+u64 get_cpu_idle_time(unsigned int cpu, u64 *wall, int io_busy);
 int cpufreq_get_policy(struct cpufreq_policy *policy, unsigned int cpu);
 int cpufreq_update_policy(unsigned int cpu);
 
@@ -347,6 +348,9 @@ static inline unsigned int cpufreq_quick_get_max(unsigned int cpu)
 }
 #endif
 
+enum {
+	BOOT_CPU = 0,
+};
 
 /*********************************************************************
  *                       CPUFREQ DEFAULT GOVERNOR                    *
@@ -441,3 +445,4 @@ void cpufreq_frequency_table_put_attr(unsigned int cpu);
 void acct_update_power(struct task_struct *p, cputime_t cputime);
 
 #endif /* _LINUX_CPUFREQ_H */
+
